58f690519c8ca474f758b60da7a39e1a80aa08ba
Tran Dinh Dong <vanvuvuong3@gmail.com>
2021-08-01 21:40:42 +0700

    update json tools code

--- json2sql/models/coin_map.py
+++ json2sql/models/coin_map.py
@@ -1,4 +1,4 @@
-from utils.sql import Connection
+from json2sql.utils.sql import Connection
 import sqlalchemy
 
 
@@ -7,7 +7,26 @@ Base = connection.init_base()
 
 
 # THIS CODE USE THE DYNAMIC CLASS
-def constructure(self, **kwargs):
-	self.kwargs = kwargs
+# constructor
+def constructor(self, **args):
+	self.args = args
 
+# method
+def method(self, arg):
+	print(arg)
 
+# class method
+@classmethod
+def class_method(cls, arg):
+	print(arg)
+
+dynamic_class = type("DynamicClass", (object, ),
+		{
+	# constructor
+	"__init__": constructor,
+	# properties
+	"type": "column",
+	# functions and method
+	"func_arg": method,
+	"class_func": class_method
+	})
