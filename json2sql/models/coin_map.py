847c92333c2861c1ada39f8380b86a49ea1a9ba6
Tran Dinh Dong <vanvuvuong3@gmail.com>
2021-08-01 22:11:46 +0700

    update the dynamic models

--- json2sql/models/coin_map.py
+++ json2sql/models/coin_map.py
@@ -6,26 +6,38 @@ connection = Connection()
 Base = connection.init_base()
 
 
-# THIS CODE USE THE DYNAMIC CLASS
+def get_column(data):
+	columns = data.keys()
+	result = []
+	for column in columns:
+		globals()[column] = sqlalchemy.Column(column,sqlalchemy.Text, nullable=True)
+		result.append(globals()[column])
+	params = tuple(result)
+	return params
+
+# THIS CODE USE THE MODEL DYNAMIC CLASS
 # constructor
 def constructor(self, **args):
-	self.args = args
+	for column in args:
+		self.column = column
 
 # method
 def method(self, arg):
+	# NO NEED FOR METHOD
 	print(arg)
 
 # class method
 @classmethod
 def class_method(cls, arg):
+	# NO NEED FOR CLASS METHOD
 	print(arg)
 
-dynamic_class = type("DynamicClass", (object, ),
+Model = type("Model", (Base, ),
 		{
 	# constructor
 	"__init__": constructor,
 	# properties
-	"type": "column",
+	"type": "Dynamic Instance",
 	# functions and method
 	"func_arg": method,
 	"class_func": class_method
